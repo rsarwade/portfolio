1913b224a71c083af656d77b791e9706bf1eec43
Rajdatta Sarwade <rajdattatestemail@gmail.com>
2019-02-26 17:48:29 +0530

    first commit

---
+++ themes/rajdatta_sarwade/assets/js/custom.js
@@ -0,0 +1,11 @@
+/*
+ * Custom JS integration
+ * Copyright 2018 Rajdatta Sarwade
+ * Contributing Author: Rajdatta Sarwade
+ */
+;
+(function ($) {
+  $('.block-views .content .flexslider').flexslider({
+    animation: "slide"
+  });
+})(jQuery);
